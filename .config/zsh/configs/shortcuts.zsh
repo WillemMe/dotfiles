b3acfcb771ecc0d628d496c5f6764b2338ae50f5
Willem <willem@bottled.tech>
2023-03-04 01:01:36 +0100

    initial

---
+++ .config/zsh/configs/shortcuts.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# Directory's
+hash -d \
+	cs="$HOME/The Bottle/Courses/" \
+	cf="$HOME/.config/" \
+	eww="$HOME/.config/eww" \
+	shell="$HOME/.config/zsh/configs" \
+	old="/mnt/home/willem"\
+
+#Files
+alias \
+	hcf="$EDITOR $XDG_CONFIG_HOME/hypr/hyprland.conf"\
